36deac918380d86c9a642dee3a9f367464a6558a
mbuguap928 <mbuguap928@gmail.com>
2022-01-20 10:09:07 +0300

    modified header.js file

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ function Header() {
                     <h1>Kanban</h1>
                     <div className="pt-3 pb-3">
                         <ul className="nav">
-                            <li className="nav-item" onClick={()=>{history.push("/");}}>Dashboard</li> |
+                            <li className="nav-item" onClick={()=>{history.push("/");}}>Dashboard</li>|
                             <li className="nav-item" onClick={()=>{history.push("/kanban");}}>Kanban</li>
                         </ul>
                     </div>
